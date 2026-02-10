68ea95114c82e2cfe95dca7f54965ee87657bd6d
Aayurt <aayurtshrestha@gmail.com>
2026-02-10 13:55:12 +0545

    Chand

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260210_080957 from './20260210_080957';
+
+export const migrations = [
+  {
+    up: migration_20260210_080957.up,
+    down: migration_20260210_080957.down,
+    name: '20260210_080957'
+  },
+];
